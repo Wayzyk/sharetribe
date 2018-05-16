e0f2c237902a8510c4b00078a3289c0343fbcb23
Boyan Tabakov <boyan@sharetribe.com>
2018-05-16 14:13:00 +0300

    fixup! Improve user deletion - clean all personal data

--- app/models/person/concerns/delete_person.rb
+++ app/models/person/concerns/delete_person.rb
@@ -32,7 +32,7 @@ module DeletePerson
           person.emails.destroy_all
 
           # Delete location
-          person.location.destroy
+          person.location.destroy if person.location
 
           # Delete avatar
           person.image.destroy
